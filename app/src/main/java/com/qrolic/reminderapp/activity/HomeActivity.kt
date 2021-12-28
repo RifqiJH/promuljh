387895e1f69b7f9f8d3f2d189f3b71c11f4b5544
RifqiJH <82458316+RifqiJH@users.noreply.github.com>
2021-12-28 07:38:39 +0700

    first commit

---
+++ app/src/main/java/com/qrolic/reminderapp/activity/HomeActivity.kt
@@ -0,0 +1,85 @@
+package com.qrolic.reminderapp.activity
+
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.material.navigation.NavigationView
+import com.qrolic.reminderapp.BuildConfig
+import com.qrolic.reminderapp.R
+import com.qrolic.reminderapp.util.MARKET_URL
+import com.qrolic.reminderapp.util.PLAYSTORE_BASE_URL
+
+
+class HomeActivity : AppCompatActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        drawerLayout = findViewById(R.id.drawer_layout)
+        val navView: NavigationView = findViewById(R.id.nav_view)
+        val navController = findNavController(R.id.nav_host_fragment)
+        val adRequest = AdRequest.Builder().build()
+
+
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.nav_home,
+                R.id.nav_calendar,
+                R.id.nav_done,
+                R.id.nav_over_due,
+                R.id.nav_today,
+                R.id.nav_tomorrow,
+                R.id.nav_upcoming,
+                R.id.nav_settings
+
+
+
+            ), drawerLayout
+        )
+
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+
+        var menu = navView.menu
+
+
+
+
+
+
+
+
+
+    }
+    override fun onBackPressed() {
+        if (this.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            this.drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+}
